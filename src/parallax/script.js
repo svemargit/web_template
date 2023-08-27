65b6448abac99ea7991e1b4ace4d008c7f38e439
Martin Švejda <svejdam@icloud.com>
2023-08-27 21:43:18 +0200

    parallax

--- src/parallax/script.js
+++ src/parallax/script.js
@@ -1,15 +1,14 @@
 let introOne = document.getElementById('introOne');
 let introTwo = document.getElementById('introTwo');
-/*let blueBack = document.getElementById('blueBack');*/
-let diver = document.getElementById('diver');
-let coralReef = document.getElementById('coralReef');
+let divers = document.getElementById('divers')
+let reef = document.getElementById('reef');
 let coral = document.getElementById('coral');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-
     introOne.style.marginLeft = value * 2.5 + 'px';
     introTwo.style.marginLeft = value * 1.8 + 'px';
-    diver.style.marginTop = value * 0.4 + 'px';
-    coral.style.marginLeft = value * (-0.1) + 'px';
+    divers.style.marginLeft = value * (-1.5) + 'px';
+    reef.style.marginTop = value * 0.4 + 'px';
+    coral.style.marginLeft = value * (-0.2) + 'px';
 });
